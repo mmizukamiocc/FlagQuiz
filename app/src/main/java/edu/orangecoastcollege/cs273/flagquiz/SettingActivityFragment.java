94e3deafb9f988e990a7e9bbb67796678d94f805
mmizukamiocc <jplockon006@gmail.com>
2016-09-28 21:47:23 -0700

    in progress

--- app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/SettingActivityFragment.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/SettingActivityFragment.java
@@ -1,5 +1,7 @@
 package edu.orangecoastcollege.cs273.flagquiz;
 
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,14 +11,11 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class SettingActivityFragment extends Fragment {
-
-    public SettingActivityFragment() {
-    }
+public class SettingActivityFragment extends PreferenceFragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
